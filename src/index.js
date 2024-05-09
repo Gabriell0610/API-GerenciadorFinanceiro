466482e832daac84478c608bf5cbd19a25e4f686
Gabriel Barbosa <gabrielbarbosaa432@gmail.com>
2024-05-09 09:03:08 -0300

    correção

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { server } from "./server/server.js";
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+server.listen(process.env.PORT, () => {
+  console.log(`App rodando na porta ${process.env.PORT}`);
 });
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,28 +1,12 @@
-// const {
-//   post,
-//   get,
-//   getId,
-//   update,
-//   remove,
-// } = require("../controller/UserController");
-
-// exports.userRoutes = (app) => {
-//   app.post("/user", post);
-//   app.get("/user", get);
-//   app.get("/user/:id", getId);
-//   app.put("/user/:id", update);
-//   app.delete("/user/:id", remove);
-// };
-
 import { Router } from "express";
-import UserControler from "../controller/UserController.js"
+import UserControler from "../controller/UserController.js";
 
 const router = Router();
 
 router.post("/user", UserControler.post);
 router.get("/user", UserControler.getAll);
 router.get("/user/:id", UserControler.getId);
-router.put("/user/:id",UserControler.update);
-router.delete("/user/:id",UserControler.remove);
+router.put("/user/:id", UserControler.update);
+router.delete("/user/:id", UserControler.remove);
 
 export { router };
